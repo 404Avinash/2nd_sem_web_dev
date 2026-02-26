82fe341d91f656605671ffb077ef155e65332325
rudrakshtyagi1 <avinash1807007@gmail.com>
2026-02-26 11:30:12 +0530

    Add index.js - Main Node.js application file

---
+++ index.js
@@ -0,0 +1,13 @@
+// Main Node.js file
+console.log("Welcome to the Node.js application!");
+
+// Example: Basic HTTP server (uncomment to use)
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World!\n');
+// });
+// server.listen(3000, () => {
+//   console.log('Server running at http://localhost:3000/');
+// });
